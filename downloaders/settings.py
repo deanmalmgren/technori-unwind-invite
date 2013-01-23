dfecf465ea867d47f67017414d6c177b94485c08
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2013-01-22 19:03:33 -0600

    addd some pipelines for processing the data

--- downloaders/settings.py
+++ downloaders/settings.py
@@ -11,9 +11,17 @@ BOT_NAME = 'technori'
 SPIDER_MODULES = [
     'technori.spiders'
 ]
+NEWSPIDER_MODULE = 'technori.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'technori (+http://www.yourdomain.com)'
+
+ITEM_PIPELINES = [
+    'technori.pipelines.ReplaceSpecialCharacters',
+    'technori.pipelines.RemoveWhitespace',
+    'technori.pipelines.RemovePunctuation',
+    'technori.pipelines.LowerCase',
+]
 
 # use a local cache instead of hitting the webpage during debugging
 HTTPCACHE_ENABLED = True
--- downloaders/technori/pipelines.py
+++ downloaders/technori/pipelines.py
@@ -3,6 +3,30 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/topics/item-pipeline.html
 
-class TechnoriPipeline(object):
+import re
+import unicodedata
+class ReplaceSpecialCharacters(object):
+    
     def process_item(self, item, spider):
+        # item["text"] = item["text"].replace(u'\xa0', ' ')
+        # # item["text"] = item["text"].encode('ascii', errors="ignore")
+        item["text"] = unicodedata.normalize('NFKD', item["text"])\
+            .encode('ascii','ignore')
+        return item
+
+class RemoveWhitespace(object):
+    whitespace_re = re.compile(r"\s+")
+    def process_item(self, item, spider):
+        item["text"] = self.whitespace_re.sub(' ', item["text"])
+        return item
+
+class RemovePunctuation(object):
+    punctuation_re = re.compile(r"[^\w\s]")
+    def process_item(self, item, spider):
+        item["text"] = self.punctuation_re.sub('', item["text"])
+        return item
+
+class LowerCase(object):
+    def process_item(self, item, spider):
+        item["text"] = item["text"].lower()
         return item
